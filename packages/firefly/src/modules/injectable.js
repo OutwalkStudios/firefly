da3c8aefa1ea42ac71b9510623138260ba723785
jleeson <jleeson2000@gmail.com>
2024-04-13 12:25:01 -0700

    removed the need for a utils folder

--- packages/firefly/src/modules/injectable.js
+++ packages/firefly/src/modules/injectable.js
@@ -1,5 +1,3 @@
-import { toPascalCase } from "../utils/string";
-
 /* a decorator to apply the injectable metadata to a class */
 export function Injectable() {
     return (target) => { target._meta = { injectable: true, name: target.name } };
@@ -8,7 +6,8 @@ export function Injectable() {
 /* a decorator to apply metadata for dependency injection on a class property */
 export function Inject(injectable) {
     const injectableKey = (typeof injectable == "function") ? injectable.name : injectable;
-    
+    const toPascalCase = (string) => string.replace(/(?:^\w|\b\w)/g, (match) => match.toUpperCase());
+
     return (target, key) => {
         target._meta = target._meta ?? { injected: [], routes: [] };
         target._meta.injected.push({ propertyKey: key, injectableKey: injectableKey ?? toPascalCase(key) });
--- packages/firefly/src/utils/string.js
+++
@@ -1,4 +0,0 @@
-/* convert a string to pascal case */
-export function toPascalCase(string) {
-    return string.replace(/(?:^\w|\b\w)/g, (match) => match.toUpperCase());
-}
