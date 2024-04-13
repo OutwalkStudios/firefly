f5941b684064ae8a2b87be521c518a26b5fec782
jleeson <jleeson2000@gmail.com>
2024-04-12 17:51:46 -0700

    cleaned up application logic

--- packages/firefly/src/index.js
+++ packages/firefly/src/index.js
@@ -4,29 +4,34 @@ import path from "path";
 
 export class Application {
 
-    app = express();
 
     constructor(options = {}) {
+        this.routes = options.routes ?? path.join(process.cwd(), "dist/routes");
         this.database = options.database;
         this.port = options.port ?? (process.env.PORT ?? 8080);
+
+        this.app = express();
 
         this.app.disable("x-powered-by");
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
     }
 
+    /* connect a middleware function to the application */
     use(middleware) {
         this.app.use(middleware);
     }
 
-    async listen(callback) {
+    async listen() {
+        /* if a database driver is provided, run the connect method */
         if (this.database) await this.database.connect();
 
-        const routes = path.join(process.cwd(), "dist/routes");
-        const injectables = await loadInjectables(routes);
-        const controllers = await loadControllers(routes, injectables);
+        /* load the injectables and controllers from the filesystem */
+        const injectables = await loadInjectables(this.routes);
+        const controllers = await loadControllers(this.routes, injectables);
         Object.entries(controllers).forEach(([route, router]) => this.app.use(route, router));
 
+        /* setup default error handling */
         this.app.use((error, req, res, next) => {
             const statusCode = error.statusCode ?? 500;
             const message = error.message ?? "Something went wrong.";
@@ -34,7 +39,8 @@ export class Application {
             return res.status(statusCode).json({ message });
         });
 
-        this.app.listen(this.port, callback);
+        /* start the web server */
+        this.app.listen(this.port);
     }
 }
 
