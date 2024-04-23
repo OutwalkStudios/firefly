ea006dc893ff200902538b33e6659efb715d7b86
jleeson <jleeson2000@gmail.com>
2024-04-23 16:39:54 -0700

    implemented the start command

--- packages/firefly/src/cli/cmds/start.js
+++ packages/firefly/src/cli/cmds/start.js
@@ -1,14 +1,36 @@
 import chalk from "chalk";
+import nodemon from "nodemon";
+import { execSync } from "child_process";
+import build from "./build";
+import path from "path";
+import fs from "fs";
 
 /* build the project for production */
 export default async function start(args) {
+    const isDev = (args.dev || args.d);
 
     /* set the node environment */
-    process.env.NODE_ENV = args.dev ? "development" : "production";
+    process.env.NODE_ENV = isDev ? "development" : "production";
 
     try {
+        const { main } = JSON.parse(fs.readFileSync(path.join(process.cwd(), "package.json")));
+        const flags = ["-r dotenv/config"];
+
+        if (!isDev) {
+            return execSync(`node ${flags.join(" ")} ${main}`, { stdio: "inherit" });
+        }
+
+        flags.push(`--watch ${main}`);
+
+        Promise.all([
+            build(args),
+            nodemon(`${flags.join(" ")} ${main}`)
+        ]);
+
+        nodemon.on("crash", () => console.log(`${chalk.red("[firefly]")} - failed to reload the application.`));
+        nodemon.on("quit", () => process.exit());
 
     } catch (error) {
-        console.error(chalk.red(`[ERROR]: ${error.message}`));
+        console.error(`${chalk.red("[firefly]")} - ${error.message}`);
     }
 }
