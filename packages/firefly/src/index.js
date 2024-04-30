e2916db9b138e21a01c75f1c32aca773dc3304a7
jleeson <jleeson2000@gmail.com>
2024-04-29 22:17:28 -0700

    improved error formatting

--- packages/firefly/src/index.js
+++ packages/firefly/src/index.js
@@ -1,4 +1,5 @@
 import { loadInjectables, loadControllers } from "./modules/core/router";
+import chalk from "chalk";
 import path from "path";
 import fs from "fs";
 
@@ -10,26 +11,30 @@ export class Application {
     }
 
     async listen(port = process.env.PORT ?? 8080) {
-        /* determine the location to load routes from */
-        const { main } = JSON.parse(fs.readFileSync(path.join(process.cwd(), "package.json")));
-        const root = main.split("/")[0];
+        try {
+            /* determine the location to load routes from */
+            const { main } = JSON.parse(fs.readFileSync(path.join(process.cwd(), "package.json")));
+            const root = main.split("/")[0];
 
-        /* if a database driver is provided, run the connect method */
-        if (this.database) await this.database.connect();
+            /* if a database driver is provided, run the connect method */
+            if (this.database) await this.database.connect();
 
-        /* load the injectables and controllers from the filesystem */
-        const injectables = await loadInjectables(root);
-        const controllers = await loadControllers(root, root, injectables);
+            /* load the injectables and controllers from the filesystem */
+            const injectables = await loadInjectables(root);
+            const controllers = await loadControllers(root, root, injectables);
 
-        Object.entries(controllers).forEach(([route, { middleware, routes }]) => {
-            this.platform.loadController(route, middleware, routes);
-        });
+            Object.entries(controllers).forEach(([route, { middleware, routes }]) => {
+                this.platform.loadController(route, middleware, routes);
+            });
 
-        /* setup the platform error handling */
-        this.platform.loadErrorHandler();
+            /* setup the platform error handling */
+            this.platform.loadErrorHandler();
 
-        /* start the web server */
-        this.platform.listen(port);
+            /* start the web server */
+            this.platform.listen(port);
+        } catch (error) {
+            console.error(`${chalk.red("[firefly]")} - ${error.message}`);
+        }
     }
 }
 
