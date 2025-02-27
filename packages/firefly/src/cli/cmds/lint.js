06fbfc166784c1d8d8d3bdfa7236556b5395ff07
jleeson <jleeson2000@gmail.com>
2025-02-27 10:34:43 -0800

    fixed lint formatting

--- packages/firefly/src/cli/cmds/lint.js
+++ packages/firefly/src/cli/cmds/lint.js
@@ -2,19 +2,19 @@ import { ESLint } from "eslint";
 import { logger } from "../../utils/logging";
 
 export default async function lint(args) {
-    const eslint = new ESLint({ fix: args.fix });
+    const eslint = new ESLint({ fix: (args.fix || args.f) });
 
     try {
         logger.log("linting the project...");
 
         const results = await eslint.lintFiles(["src/**/*.{js,ts}"]);
 
-        if (args.fix) await ESLint.outputFixes(results);
+        if ((args.fix || args.f)) await ESLint.outputFixes(results);
 
         const formatter = await eslint.loadFormatter("stylish");
         const resultText = formatter.format(results);
 
-        console.log(resultText);
+        if (resultText.trim().length > 0) console.log(resultText);
 
         return { isError: (results.filter((result) => result.errorCount > 0).length > 0) };
     } catch (error) {
